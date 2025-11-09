02ecd1efacffb7911e99c9e51994ac891f3ccf91
Joaquín Vallejos <joaquinnqn989@gmail.com>
2025-11-09 18:06:16 -0300

    Feat: Validacion JS

---
+++ Paginas(HTML)/Archivos(JavaScript)/JavaScript_Contacto_Joaquin.js
@@ -0,0 +1,68 @@
+const formulario = document.getElementById('formulario');
+const inputs = document.querySelectorAll('#formulario input, #formulario textarea');
+
+// Expresiones regulares
+const expresiones = {
+  n: /^[a-zA-ZÀ-ÿ\s]{3,40}$/,
+  a: /^[a-zA-ZÀ-ÿ\s]{3,40}$/,
+  correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/, 
+  op: /^.{10,300}$/ 
+};
+
+const campos = {
+  n: false,
+  a: false,
+  correo: false,
+  op: false
+};
+
+const validarFormulario = (e) => {
+  switch (e.target.name) {
+    case "n":
+      validarCampo(expresiones.n, e.target, 'nom');
+      break;
+    case "a":
+      validarCampo(expresiones.a, e.target, 'ape');
+      break;
+    case "correo":
+      validarCampo(expresiones.correo, e.target, 'correo');
+      break;
+    case "op":
+      validarCampo(expresiones.op, e.target, 'op');
+      break;
+  }
+};
+
+const validarCampo = (expresion, input, campo) => {
+  const grupo = document.getElementById(`grupo__${campo}`);
+  if (expresion.test(input.value)) {
+    grupo.classList.remove('form__grupo-incorrecto');
+    grupo.classList.add('form__grupo-correcto');
+    grupo.querySelector('.form__input-error').classList.remove('form__input-error-activo');
+    campos[input.name] = true;
+  } else {
+    grupo.classList.add('form__grupo-incorrecto');
+    grupo.classList.remove('form__grupo-correcto');
+    grupo.querySelector('.form__input-error').classList.add('form__input-error-activo');
+    campos[input.name] = false;
+  }
+};
+
+inputs.forEach((input) => {
+  input.addEventListener('keyup', validarFormulario);
+  input.addEventListener('blur', validarFormulario);
+});
+
+formulario.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  if (campos.n && campos.a && campos.correo && campos.op) {
+    alert('Opinión enviada con éxito');
+    formulario.reset();
+    document.querySelectorAll('.form__grupo-correcto').forEach((grupo) => {
+      grupo.classList.remove('form__grupo-correcto');
+    });
+  } else {
+    alert('Por favor completá todos los campos correctamente');
+  }
+});
--- Paginas(HTML)/Archivos(JavaScript)/Javascript_Prototipo_Gaspar.js
+++ Paginas(HTML)/Archivos(JavaScript)/Javascript_Prototipo_Gaspar.js
@@ -28,3 +28,91 @@ let carrito = [];
 
       document.getElementById('total').textContent = 'Total: $' + total;
     }
+
+
+const formulario = document.getElementById('formulario');
+const inputs = document.querySelectorAll('#formulario input');
+
+const expresiones = {
+  n: /^[a-zA-ZÀ-ÿ\s]{3,40}$/, 
+  a: /^[a-zA-ZÀ-ÿ\s]{3,40}$/, 
+  correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+  d: /^.{5,60}$/,              
+  c: /^\d{4}$/,               
+  t: /^\d{16}$/,               
+  cs: /^\d{3}$/                
+};
+
+const campos = {
+  n: false,
+  a: false,
+  d: false,
+  c: false,
+  t: false,
+  cs: false
+};
+
+const validarFormulario = (e) => {
+  switch (e.target.name) {
+    case "n":
+      validarCampo(expresiones.n, e.target, 'nom');
+      break;
+    case "a":
+      validarCampo(expresiones.a, e.target, 'ape');
+      break;
+    case "d":
+      validarCampo(expresiones.d, e.target, 'dir');
+      break;
+    case "c":
+      validarCampo(expresiones.c, e.target, 'cod');
+      break;
+    case "t":
+      validarCampo(expresiones.t, e.target, 'tar');
+      break;
+    case "cs":
+      validarCampo(expresiones.cs, e.target, 'segu');
+      break;
+    case "correo":
+      validarCampo(expresiones.correo, e.target, 'correo');
+        break;
+    case "op":
+      validarCampo(expresiones.op, e.target, 'op');
+  break;
+  }
+};
+
+const validarCampo = (expresion, input, campo) => {
+  const grupo = document.getElementById(`grupo__${campo}`);
+  if (expresion.test(input.value)) {
+    grupo.classList.remove('form__grupo-incorrecto');
+    grupo.classList.add('form__grupo-correcto');
+    grupo.querySelector('.form__input-error').classList.remove('form__input-error-activo');
+    campos[input.name] = true;
+  } else {
+    grupo.classList.add('form__grupo-incorrecto');
+    grupo.classList.remove('form__grupo-correcto');
+    grupo.querySelector('.form__input-error').classList.add('form__input-error-activo');
+    campos[input.name] = false;
+  }
+};
+
+inputs.forEach((input) => {
+  input.addEventListener('keyup', validarFormulario);
+  input.addEventListener('blur', validarFormulario);
+});
+
+
+formulario.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const terminos = document.getElementById('terminos');
+  if (campos.n && campos.a && campos.d && campos.c && campos.t && campos.cs && terminos.checked) {
+    alert('Compra realizada');
+    formulario.reset();
+    document.querySelectorAll('.form__grupo-correcto').forEach((grupo) => {
+      grupo.classList.remove('form__grupo-correcto');
+    });
+  } else {
+    alert('Por favor completá todos los campos correctamente');
+  }
+});
